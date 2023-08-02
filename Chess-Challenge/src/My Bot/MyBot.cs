426919798068e84e626d6bbaa0d41dda6fb01242
Silas Maughan <silas.maughan@gmail.com>
2023-08-02 12:30:44 +0200

    Board Evaluation

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,18 +1,40 @@
-﻿using ChessChallenge.API;
+﻿using System.Collections;
+using System.Collections.Generic;
+using ChessChallenge.API;
+using System.Linq;
+using System;
 
 public class MyBot : IChessBot
 {
+    public int pv_calc(Board board, Move move) {
+        board.MakeMove(move);
+        PieceList[] piecelistArr = board.GetAllPieceLists();
+
+        for (int i = 0; i < piecelistArr.Length; i++)
+        {
+            PieceList piecelist = piecelistArr[i];
+            for (int j = 0; j < piecelist.Count; j++)
+            {
+                Piece cur_piece = piecelist.GetPiece(j);
+                Console.WriteLine(cur_piece);
+            }
+        }
+        return 3;
+    } 
+    
     public Move Think(Board board, Timer timer)
     {
-        Move[] capture_moves = board.GetLegalMoves(true);
-        if (capture_moves.Length > 0)
+        Move[] pos_moves = board.GetLegalMoves();
+        List<int> mov_pv = new List<int>();
+
+        for (int i = 0; i < pos_moves.Length; i++)
         {
-            return capture_moves[0];
+            mov_pv.Add(pv_calc(board, pos_moves[i]));
         }
-        else
-        {
-            Move[] moves = board.GetLegalMoves();
-            return moves[0];
-        }
+        int maxPv = mov_pv.Max();
+        int maxPvIndex = mov_pv.IndexOf(maxPv);
+        return pos_moves[maxPvIndex];
     }
+
+
 }
