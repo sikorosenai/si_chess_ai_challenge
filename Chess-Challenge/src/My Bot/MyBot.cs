e084d0ba49c3774f57dc74be6fa206ef723911a8
Silas Maughan <silas.maughan@gmail.com>
2023-08-02 10:59:41 +0200

    If attack pos, attack

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,7 +4,15 @@ public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        Move[] capture_moves = board.GetLegalMoves(true);
+        if (capture_moves.Length > 0)
+        {
+            return capture_moves[0];
+        }
+        else
+        {
+            Move[] moves = board.GetLegalMoves();
+            return moves[0];
+        }
     }
 }
